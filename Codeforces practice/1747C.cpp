1fcf8f6e7dc2a6d9130c64ab1ccc10b135f64077
Rishabh-Agarwal9 <rishabha1233@gmail.com>
2024-08-19 15:25:39 +0530

    constructive

---
+++ Codeforces practice/1747C.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+typedef set<string> ss;
+typedef vector<int> vi;
+typedef map<int, char> msi;
+typedef pair<int, int> pa;
+typedef long long int ll;
+
+ll gcd (ll a, ll b) {
+    return b ? gcd (b, a % b) : a;
+}
+//mmi
+ll inv(ll a) {
+  return a <= 1 ? a : mod - (ll)(mod/a) * inv(mod % a) % mod;
+}
+
+//make a fact vect filled to maxN you want to query.
+long long binomial_coefficient(ll n, ll k, vector<ll> &fact) {
+    if(k>n) return 0;
+    return fact[n] * inv(fact[k] * fact[n - k] % mod) % mod;
+}
+
+void print(vi &a){
+    for(auto x : a){
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> vect(n);
+    for(int i=0;i<n;i++){
+        cin>>vect[i];
+    }
+    ll mini = LLONG_MAX;
+    for(int i=1;i<n;i++){
+        mini = min(mini, vect[i]);
+    }
+
+    if(vect[0]<=mini){
+        cout<<"BOB"<<"\n";
+        return;
+    }
+    cout<<"ALICE"<<"\n";
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases;
+    cin >> cases;
+    while (cases--){
+        solve();    
+    }
+    return 0;
+}
