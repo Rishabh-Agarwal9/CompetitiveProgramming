2b403eff5e02e192351921d6430440cae973b505
Rishabh-Agarwal9 <rishabha1233@gmail.com>
2024-08-27 13:22:56 +0530

    greedy

---
+++ Codeforces practice/1717C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+typedef set<string> ss;
+typedef vector<int> vi;
+typedef map<int, char> msi;
+typedef pair<int, int> pa;
+typedef long long int ll;
+
+ll gcd (ll a, ll b) {
+    return b ? gcd (b, a % b) : a;
+}
+//mmi
+ll inv(ll a) {
+  return a <= 1 ? a : mod - (ll)(mod/a) * inv(mod % a) % mod;
+}
+
+//make a fact vect filled to maxN you want to query.
+long long binomial_coefficient(ll n, ll k, vector<ll> &fact) {
+    if(k>n) return 0;
+    return fact[n] * inv(fact[k] * fact[n - k] % mod) % mod;
+}
+
+void print(vi &a){
+    for(auto x : a){
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    vector<ll> b(n);
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    }
+
+    for(int i=n-1;i>=0;i--){
+        if(b[i]<a[i]){
+            cout<<"NO"<<"\n";
+            return;
+        }
+        if(a[i]!=b[i] && b[i]>b[(i+1)%n]+1){
+            cout<<"NO"<<"\n";
+            return;
+        }
+    }
+    cout<<"YES"<<"\n";
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases;
+    cin >> cases;
+    while (cases--){
+        solve();    
+    }
+    return 0;
+}
