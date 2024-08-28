fcf07a4317052d93c76a30938ab3ab6cb99cee7b
Rishabh-Agarwal9 <rishabha1233@gmail.com>
2024-08-28 13:22:15 +0530

    binary search

---
+++ Codeforces practice/1883F.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+typedef set<string> ss;
+typedef vector<int> vi;
+typedef map<int, char> msi;
+typedef pair<int, int> pa;
+typedef long long int ll;
+
+ll gcd (ll a, ll b) {
+    return b ? gcd (b, a % b) : a;
+}
+//mmi
+ll inv(ll a) {
+  return a <= 1 ? a : mod - (ll)(mod/a) * inv(mod % a) % mod;
+}
+
+//make a fact vect filled to maxN you want to query.
+long long binomial_coefficient(ll n, ll k, vector<ll> &fact) {
+    if(k>n) return 0;
+    return fact[n] * inv(fact[k] * fact[n - k] % mod) % mod;
+}
+
+void print(vi &a){
+    for(auto x : a){
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> vect(n);
+    for(int i=0;i<n;i++){
+        cin>>vect[i];
+    }
+    vector<ll> start;
+    vector<ll> end;
+    set<ll> st;
+
+    for(int i=0;i<n;i++){
+        if(st.find(vect[i])==st.end()){
+            start.push_back(i);
+        }
+        st.insert(vect[i]);
+    }
+    st.clear();
+    for(int i=n-1;i>=0;i--){
+        if(st.find(vect[i])==st.end()){
+            end.push_back(i);
+        }
+        st.insert(vect[i]);
+    }
+    reverse(end.begin(), end.end());
+    ll ans =0;
+
+    for(int i=0;i<start.size();i++){
+        ll l =-1;
+        ll r = end.size();
+        while(r>l+1){
+            ll mid = (r+l)/2;
+            if(start[i] <= end[mid]){
+                r=mid;
+            }
+            else{
+                l=mid;
+            }
+        }
+        // cout<<r<<" ";
+        ans+=end.size()-r;
+    }
+
+    cout<<ans<<"\n";
+
+}   
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases;
+    cin >> cases;
+    while (cases--){
+        solve();    
+    }
+    return 0;
+}
